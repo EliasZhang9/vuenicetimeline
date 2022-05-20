3931a9f0c1aa3bb883aa81b9b162090e5376305e
Yili <eliaszhang@hotmail.com>
2022-05-20 22:48:21 +0200

    commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import Vuenicetimeline from '@/vuenicetimeline.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Vuenicetimeline
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vuenicetimeline />
+  </div>
+</template>
